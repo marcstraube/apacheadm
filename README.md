9402e849b5d1c6b79e1d9574c751a634283a1720
Marc Straube <email@marcstraube.de>
2020-10-29 01:41:53 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# apacheadm
+
+Tool to enable and disable modules and vhosts for Apache.
+
+
+## Install
+
+./install.sh
+
+
+### Options
+
+-d Destination directory
+
+-c Config directory
+
---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if [ ${EUID} -ne 0 ]; then
+  printf "Install script must be run as root!\n"
+fi
+
+DESTDIR=""
+CONFDIR="/etc/httpd"
+
+while getopts d:c: option; do
+  case "${option}" in
+    c) CONFDIR=${OPTARG};;
+    d) DESTDIR=${OPTARG};;
+  esac
+done
+
+install -d ${DESTDIR}${CONFDIR}/conf/mods-available/
+install -d ${DESTDIR}${CONFDIR}/conf/mods-enabled/
+install -d ${DESTDIR}${CONFDIR}/conf/sites-available/
+install -d ${DESTDIR}${CONFDIR}/conf/sites-enabled/
+
+install -Dm0755 src/apacheadm ${DESTDIR}/usr/bin/apacheadm
+install -Dm0644 src/httpd.conf.example ${DESTDIR}${CONFDIR}/conf/httpd.conf.example
+install -Dm0644 src/mods-available/*.{conf,load} ${DESTDIR}${CONFDIR}/conf/mods-available/
+install -Dm0644 src/sites-available/*.conf ${DESTDIR}${CONFDIR}/conf/sites-available/
+
+default_modules=( mpm_event log_config mime dir authz_core unixd headers )
+for module in ${default_modules[@]}; do
+  ln -s ../mods-available/${module}.load ${DESTDIR}${CONFDIR}/conf/mods-enabled/${module}.load
+  if [ -e ${DESTDIR}${CONFDIR}/conf/mods-available/${module}.conf ]; then
+    ln -s ../mods-available/${module}.conf ${DESTDIR}${CONFDIR}/conf/mods-enabled/${module}.conf
+  fi
+done
+
+ln -s ../sites-available/00-default.conf ${DESTDIR}${CONFDIR}/conf/sites-enabled/00-default.conf
+
